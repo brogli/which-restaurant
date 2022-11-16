07a35e8cc49c83ff3689aa3f5d39b1f33f41e91b
brogli <alexanderbrogli@gmx.net>
2022-11-16 11:44:36 +0100

    put files

---
+++ whereFood.py
@@ -0,0 +1,14 @@
+import random
+
+restaurants: list[str] = [
+    "kaimug",
+    "Cheti's",
+    "coop to go",
+    "kaisin",
+    "tibits",
+    "tschingg",
+    "rice up",
+    "gärtnerei",
+]
+
+print(random.choice(restaurants))
